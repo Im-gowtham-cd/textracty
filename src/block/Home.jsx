9f3ed7dd8a465f39894b08893b51baf092a02101
Gowtham C D <gowthamcdstudies@gmail.com>
2025-08-08 11:06:31 +0530

    Home-left

--- src/block/Home.jsx
+++ src/block/Home.jsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom';
+import { FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div className="left">
+        <p className="left-title">Textracty</p>
+        <ul className="left-desc">
+          <li>Extracts text from images and documents</li>
+          <li>Uses advanced OCR for high accuracy</li>
+          <li>Supports multiple languages and file types</li>
+          <li>Ideal for students, professionals, businesses</li>
+        </ul>
+        <div className="left-btn">
+          <Link to={'/login'} className="btn">Login</Link>
+          <Link to={'/signup'} className="btn">Signup</Link>
+        </div>
+        <span className='left-social'>
+          <FaTwitter size={20} />
+          <FaFacebookF size={20} />
+          <FaInstagram size={20} />
+          <FaLinkedinIn size={20} />
+          <FaGithub size={20} />
+        </span>
+
+      </div>
+      <div className="right">
+
+      </div>
+    </>
   );
 }
